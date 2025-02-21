b155c4528cd55e61d69391ef0dc96bce2e400a6d
User <user@MacBookPro.lan>
2025-02-20 23:06:06 -0600

    Removing sensitive data

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
-from app.routes import users, courses
+from enum import Enum
 
+class ModelName(str, Enum):
+    alexnet = 'alexnet'
+    resnet = 'resnet'
+    lenet = 'lenet'
 app = FastAPI()
 
-app.include_router(users.router)
-app.include_router(courses.router)
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
 
-@app.get('/')
-async def root():
-    return {'message': 'Welcome to Lumber LMS'}
+    if model_name.value == 'lenet':
+        return {'model_name': model_name, 'message': "LeCNN all the images"}
 
+    return {'model_name': model_name, 'message': 'Have some residuals'}
